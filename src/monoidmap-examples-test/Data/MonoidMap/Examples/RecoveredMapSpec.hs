aa307beb11935c891b63b521368f421d110c06b9
Jonathan Knowles <mail@jonathanknowles.net>
2022-05-08 11:26:32 +0000

    Add map type that recovers the original interface of `Data.Map`.

---
+++ src/monoidmap-examples-test/Data/MonoidMap/Examples/RecoveredMapSpec.hs
@@ -0,0 +1,229 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+-- |
+-- Copyright: © 2022 Jonathan Knowles
+-- License: Apache-2.0
+--
+module Data.MonoidMap.Examples.RecoveredMapSpec
+    where
+
+import Prelude
+
+import Data.Function
+    ( on, (&) )
+import Data.List
+    ( nubBy )
+import Data.Monoid
+    ( Sum (..) )
+import Data.Monoid.Null
+    ( MonoidNull )
+import Test.Hspec
+    ( Spec, describe, it, parallel )
+import Test.QuickCheck
+    ( Arbitrary (..)
+    , Property
+    , checkCoverage
+    , cover
+    , listOf
+    , property
+    , shrinkMapBy
+    , (===)
+    )
+import Test.QuickCheck.Instances.Natural
+    ()
+
+import qualified Data.Map.Strict as OMap
+import qualified Data.MonoidMap.Examples.RecoveredMap as RMap
+import qualified Data.Set as Set
+
+spec :: Spec
+spec = do
+
+    parallel $ describe "Conversion to and from lists" $ do
+        it "prop_fromList_toList" $
+            prop_fromList_toList & property
+
+    parallel $ describe "Empty" $ do
+        it "prop_empty_keysSet" $
+            prop_empty_keysSet & property
+        it "prop_empty_lookup" $
+            prop_empty_lookup & property
+        it "prop_empty_show" $
+            prop_empty_show & property
+        it "prop_empty_toList" $
+            prop_empty_toList & property
+
+    parallel $ describe "Singleton" $ do
+        it "prop_singleton_keysSet" $
+            prop_singleton_keysSet & property
+        it "prop_singleton_lookup" $
+            prop_singleton_lookup & property
+        it "prop_singleton_show" $
+            prop_singleton_show & property
+        it "prop_singleton_toList" $
+            prop_singleton_toList & property
+
+    parallel $ describe "Append" $ do
+        it "prop_append_toList" $
+            prop_append_toList & property
+
+    parallel $ describe "Insert" $ do
+        it "prop_insert_lookup" $
+            prop_insert_lookup & property
+        it "prop_insert_member" $
+            prop_insert_member & property
+        it "prop_insert_toList" $
+            prop_insert_toList & property
+
+--------------------------------------------------------------------------------
+-- Test types
+--------------------------------------------------------------------------------
+
+type Key = Int
+type Value = Sum Int
+
+--------------------------------------------------------------------------------
+-- Conversion to and from lists
+--------------------------------------------------------------------------------
+
+prop_fromList_toList :: [(Key, Value)] -> Property
+prop_fromList_toList kvs =
+    (===)
+        (RMap.toList (RMap.fromList kvs))
+        (OMap.toList (OMap.fromList kvs))
+    & cover 10
+        (length kvs > 1 && length (nubBy ((==) `on` fst) kvs) /= length kvs)
+        "length kvs > 1 && length (nubBy ((==) `on` fst) kvs) /= length kvs"
+    & cover 10
+        (length kvs > 1 && length (nubBy ((==) `on` fst) kvs) == length kvs)
+        "length kvs > 1 && length (nubBy ((==) `on` fst) kvs) == length kvs"
+    & checkCoverage
+
+--------------------------------------------------------------------------------
+-- Empty
+--------------------------------------------------------------------------------
+
+prop_empty_keysSet :: Property
+prop_empty_keysSet =
+    (===)
+        (RMap.keysSet (RMap.empty @Key @Value))
+        (OMap.keysSet (OMap.empty @Key @Value))
+
+prop_empty_lookup :: Key -> Property
+prop_empty_lookup k =
+    (===)
+        (RMap.lookup k (RMap.empty @Key @Value))
+        (OMap.lookup k (OMap.empty @Key @Value))
+
+prop_empty_show :: Property
+prop_empty_show =
+    (===)
+        (show (RMap.empty @Key @Value))
+        (show (OMap.empty @Key @Value))
+
+prop_empty_toList :: Property
+prop_empty_toList =
+    (===)
+        (RMap.toList (RMap.empty @Key @Value))
+        (OMap.toList (OMap.empty @Key @Value))
+
+--------------------------------------------------------------------------------
+-- Singleton
+--------------------------------------------------------------------------------
+
+prop_singleton_keysSet :: Key -> Value -> Property
+prop_singleton_keysSet k v =
+    (===)
+        (RMap.keysSet (RMap.singleton k v))
+        (OMap.keysSet (OMap.singleton k v))
+
+prop_singleton_lookup :: Key -> Value -> Property
+prop_singleton_lookup k v =
+    (===)
+        (RMap.lookup k (RMap.singleton k v))
+        (OMap.lookup k (OMap.singleton k v))
+
+prop_singleton_show :: Key -> Value -> Property
+prop_singleton_show k v =
+    (===)
+        (show (RMap.singleton k v))
+        (show (OMap.singleton k v))
+
+prop_singleton_toList :: Key -> Value -> Property
+prop_singleton_toList k v =
+    (===)
+        (RMap.toList (RMap.singleton k v))
+        (OMap.toList (OMap.singleton k v))
+
+--------------------------------------------------------------------------------
+-- Append
+--------------------------------------------------------------------------------
+
+prop_append_toList :: [(Key, Value)] -> [(Key, Value)] -> Property
+prop_append_toList kvs1 kvs2 =
+    (===)
+        (RMap.toList (RMap.fromList kvs1 <> RMap.fromList kvs2))
+        (OMap.toList (OMap.fromList kvs1 <> OMap.fromList kvs2))
+    & cover 10
+        (ks1 `Set.disjoint` ks2)
+        "ks1 `Set.disjoint` ks2"
+    & cover 10
+        (not (ks1 `Set.disjoint` ks2))
+        "not (ks1 `Set.disjoint` ks2)"
+    & checkCoverage
+  where
+    ks1 = Set.fromList (fst <$> kvs1)
+    ks2 = Set.fromList (fst <$> kvs2)
+
+--------------------------------------------------------------------------------
+-- Insert
+--------------------------------------------------------------------------------
+
+prop_insert_lookup :: [(Key, Value)] -> Key -> Value -> Property
+prop_insert_lookup kvs k v =
+    (===)
+        (RMap.lookup k (RMap.insert k v (RMap.fromList kvs)))
+        (OMap.lookup k (OMap.insert k v (OMap.fromList kvs)))
+    & cover 10
+        (filter ((== k) . fst) kvs == [])
+        "filter ((== k) . fst) kvs == []"
+    & cover 10
+        (filter ((== k) . fst) kvs /= [])
+        "filter ((== k) . fst) kvs /= []"
+    & checkCoverage
+
+prop_insert_member :: [(Key, Value)] -> Key -> Value -> Property
+prop_insert_member kvs k v =
+    (===)
+        (RMap.member k (RMap.insert k v (RMap.fromList kvs)))
+        (OMap.member k (OMap.insert k v (OMap.fromList kvs)))
+    & cover 10
+        (filter ((== k) . fst) kvs == [])
+        "filter ((== k) . fst) kvs == []"
+    & cover 10
+        (filter ((== k) . fst) kvs /= [])
+        "filter ((== k) . fst) kvs /= []"
+    & checkCoverage
+
+prop_insert_toList :: [(Key, Value)] -> Key -> Value -> Property
+prop_insert_toList kvs k v =
+    (===)
+        (RMap.toList (RMap.insert k v (RMap.fromList kvs)))
+        (OMap.toList (OMap.insert k v (OMap.fromList kvs)))
+    & cover 10
+        (filter ((== k) . fst) kvs == [])
+        "filter ((== k) . fst) kvs == []"
+    & cover 10
+        (filter ((== k) . fst) kvs /= [])
+        "filter ((== k) . fst) kvs /= []"
+    & checkCoverage
+
+--------------------------------------------------------------------------------
+-- Arbitrary instances
+--------------------------------------------------------------------------------
+
+instance (Arbitrary k, Ord k, Arbitrary v, MonoidNull v) =>
+    Arbitrary (RMap.Map k v)
+  where
+    arbitrary = RMap.fromList <$> listOf ((,) <$> arbitrary <*> arbitrary)
+    shrink = shrinkMapBy RMap.fromList RMap.toList shrink
---
+++ src/monoidmap-examples/Data/MonoidMap/Examples/RecoveredMap.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+module Data.MonoidMap.Examples.RecoveredMap where
+
+import Data.Maybe
+    ( mapMaybe )
+import Data.Monoid
+    ( First (..) )
+import Data.MonoidMap
+    ( MonoidMap )
+import Data.Set
+    ( Set )
+
+import qualified Data.MonoidMap as MonoidMap
+
+newtype Map k v = Map
+    {unMap :: MonoidMap k (First v)}
+    deriving stock Eq
+    deriving newtype (Semigroup, Monoid)
+
+instance (Show k, Show v) => Show (Map k v) where
+    show = ("fromList " <>) . show . toList
+
+empty :: Map k v
+empty = Map MonoidMap.empty
+
+singleton :: Ord k => k -> v -> Map k v
+singleton k = Map . MonoidMap.singleton k . pure
+
+fromList :: Ord k => [(k, v)] -> Map k v
+fromList = Map . MonoidMap.fromListWith const . fmap (fmap pure)
+
+toList :: Map k v -> [(k, v)]
+toList = mapMaybe (getFirst . sequenceA) . MonoidMap.toList . unMap
+
+insert :: Ord k => k -> v -> Map k v -> Map k v
+insert k v = Map . MonoidMap.insertWith const k (pure v) . unMap
+
+keysSet :: Map k v -> Set k
+keysSet = MonoidMap.keysSet . unMap
+
+lookup :: Ord k => k -> Map k v -> Maybe v
+lookup k = getFirst . MonoidMap.lookup k . unMap
+
+member :: Ord k => k -> Map k v -> Bool
+member k = MonoidMap.member k . unMap
