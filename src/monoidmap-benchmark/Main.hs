a118c5818dd3efd2714dfb8ec988f3b7480d358a
Jonathan Knowles <mail@jonathanknowles.net>
2022-05-29 07:15:16 +0000

    Use `Map` type class to remove duplication in benchmark.

--- src/monoidmap-benchmark/Main.hs
+++ src/monoidmap-benchmark/Main.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
 module Main where
 
 import Prelude hiding
@@ -20,11 +23,11 @@ import qualified Data.MonoidMap.Internal.RecoveredMap as RMap
 main :: IO ()
 main = do
 
-    let om_even = OMap.fromList elems_even :: OMap.Map Int Int
-        om_odd  = OMap.fromList elems_odd  :: OMap.Map Int Int
+    let om_even = fromList elems_even :: OMap.Map Int Int
+        om_odd  = fromList elems_odd  :: OMap.Map Int Int
 
-        rm_even = RMap.fromList elems_even :: RMap.Map Int Int
-        rm_odd  = RMap.fromList elems_odd  :: RMap.Map Int Int
+        rm_even = fromList elems_even :: RMap.Map Int Int
+        rm_odd  = fromList elems_odd  :: RMap.Map Int Int
 
     evaluate $ rnf [om_even, om_odd]
     evaluate $ rnf [rm_even, rm_odd]
@@ -32,15 +35,15 @@ main = do
     defaultMain
         [ bgroup "lookup (absent)"
             [ bench "Data.Map.Strict" $
-                nf (olookup evens) om_odd
+                nf (lookupMany evens) om_odd
             , bench "Data.MonoidMap" $
-                nf (rlookup evens) rm_odd
+                nf (lookupMany evens) rm_odd
             ]
         , bgroup "lookup (present)"
             [ bench "Data.Map.Strict" $
-                nf (olookup evens) om_even
+                nf (lookupMany evens) om_even
             , bench "Data.MonoidMap" $
-                nf (rlookup evens) rm_even
+                nf (lookupMany evens) rm_even
             ]
         ]
   where
@@ -59,8 +62,17 @@ main = do
     odds :: [Int]
     odds = [1, 3 .. bound]
 
-olookup :: [Int] -> OMap.Map Int Int -> Int
-olookup xs m = foldl' (\n k -> fromMaybe n (OMap.lookup k m)) 0 xs
+class Ord k => Map m k v where
+    fromList :: [(k, v)] -> m k v
+    lookup :: k -> m k v -> Maybe v
 
-rlookup :: [Int] -> RMap.Map Int Int -> Int
-rlookup xs m = foldl' (\n k -> fromMaybe n (RMap.lookup k m)) 0 xs
+instance Ord k => Map OMap.Map k v where
+    fromList = OMap.fromList
+    lookup = OMap.lookup
+
+instance Ord k => Map RMap.Map k v where
+    fromList = RMap.fromList
+    lookup = RMap.lookup
+
+lookupMany :: (Map m k v, Num v) => [k] -> m k v -> v
+lookupMany xs m = foldl' (\n k -> fromMaybe n (lookup k m)) 0 xs
