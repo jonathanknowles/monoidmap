de1750b139babf6600d7eb3890b1cd620cea1d46
Jonathan Knowles <mail@jonathanknowles.net>
2023-01-25 06:32:41 +0000

    Add unit tests for `overlap` with `Sum Natural`.

--- src/test/Data/MonoidMapSpec.hs
+++ src/test/Data/MonoidMapSpec.hs
@@ -496,6 +496,7 @@ specUnit = describe "Unit tests" $ do
     describe "OverlappingGCDMonoid" $ do
 
         unitTestSpec_OverlappingGCDMonoid_overlap_String
+        unitTestSpec_OverlappingGCDMonoid_overlap_Sum_Natural
 
     describe "GCDMonoid" $ do
 
@@ -2110,6 +2111,26 @@ unitTestData_OverlappingGCDMonoid_overlap_String = unitTestData2
     , ( m ["abcdefgh", "01234567"]
       , m ["abcdefgh", "01234567"]
       , m ["abcdefgh", "01234567"]
+      )
+    ]
+  where
+    m = MonoidMap.fromList . zip [A ..]
+
+unitTestSpec_OverlappingGCDMonoid_overlap_Sum_Natural :: Spec
+unitTestSpec_OverlappingGCDMonoid_overlap_Sum_Natural = unitTestSpec
+    "OverlappingGCDMonoid.overlap (Sum Natural)"
+    "overlap"
+    (overlap)
+    (unitTestData_OverlappingGCDMonoid_overlap_Sum_Natural)
+
+unitTestData_OverlappingGCDMonoid_overlap_Sum_Natural :: UnitTestData2
+    (MonoidMap LatinChar (Sum Natural))
+    (MonoidMap LatinChar (Sum Natural))
+    (MonoidMap LatinChar (Sum Natural))
+unitTestData_OverlappingGCDMonoid_overlap_Sum_Natural = unitTestData2
+    [ ( m [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+      , m [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+      , m [0, 1, 2, 3, 4, 4, 3, 2, 1, 0]
       )
     ]
   where
