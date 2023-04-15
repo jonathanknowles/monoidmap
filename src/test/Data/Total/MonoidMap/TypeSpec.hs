2bc4c8c7e1121b2a4bf6b11ed5b7d533d505c527
Jonathan Knowles <mail@jonathanknowles.net>
2023-04-15 09:49:24 +0000

    Create `SliceSpec`.

--- src/test/Data/Total/MonoidMap/TypeSpec.hs
+++ src/test/Data/Total/MonoidMap/TypeSpec.hs
@@ -14,7 +14,7 @@ module Data.Total.MonoidMap.TypeSpec
 import Prelude
 
 import Data.Bifunctor
-    ( bimap, first, second )
+    ( first, second )
 import Data.Function
     ( (&) )
 import Data.Functor.Identity
@@ -35,8 +35,6 @@ import Data.Total.MonoidMap
     ( MonoidMap, nonNullCount )
 import Data.Typeable
     ( typeRep )
-import GHC.Exts
-    ( IsList (..) )
 import Numeric.Natural
     ( Natural )
 import Test.Common
@@ -44,16 +42,12 @@ import Test.Common
 import Test.Hspec
     ( Spec, describe, it )
 import Test.QuickCheck
-    ( Arbitrary (..)
-    , Fun (..)
-    , Gen
+    ( Fun (..)
     , Property
     , applyFun
     , applyFun2
-    , choose
     , cover
     , expectFailure
-    , oneof
     , (===)
     )
 
@@ -173,17 +167,6 @@ specFor _k _v = describe (show $ typeRep (Proxy @(MonoidMap k v))) $ do
     describe "Keys" $ do
         it "prop_nonNullKeys_get" $
             prop_nonNullKeys_get
-                @k @v & property
-
-    describe "Slicing" $ do
-        it "prop_take_toList_fromList" $
-            prop_take_toList_fromList
-                @k @v & property
-        it "prop_drop_toList_fromList" $
-            prop_drop_toList_fromList
-                @k @v & property
-        it "prop_splitAt_toList_fromList" $
-            prop_splitAt_toList_fromList
                 @k @v & property
 
     describe "Mapping" $ do
@@ -646,93 +629,6 @@ prop_nonNullKeys_get m =
     & cover 2
         (not (MonoidMap.null m))
         "not (MonoidMap.null m)"
-
---------------------------------------------------------------------------------
--- Slicing
---------------------------------------------------------------------------------
-
-data Slice k v = Slice Int (MonoidMap k v)
-    deriving (Eq, Show)
-
-instance (Arbitrary k, Arbitrary v, MonoidNull v, Ord k) =>
-    Arbitrary (Slice k v)
-  where
-    arbitrary = do
-        m <- genMap
-        i <- genIndex m
-        pure $ Slice i m
-      where
-        genMap :: Gen (MonoidMap k v)
-        genMap = arbitrary
-
-        genIndex :: MonoidMap k v -> Gen Int
-        genIndex m = oneof
-            [ choose (negate (length m), -1)
-            , pure 0
-            , choose (1, length m - 1)
-            , pure (length m)
-            , choose (length m + 1, 2 * length m)
-            ]
-
-prop_take_toList_fromList
-    :: (Ord k, Show k, Eq v, MonoidNull v, Show v)
-    => Slice k v
-    -> Property
-prop_take_toList_fromList (Slice i m) =
-    MonoidMap.take i m
-        === (fromList . Prelude.take i . toList) m
-    & cover 2
-        (i == 0 && 0 < nonNullCount m)
-        "i == 0 && 0 < nonNullCount m"
-    & cover 2
-        (0 < i && i < nonNullCount m)
-        "0 < i && i < nonNullCount m"
-    & cover 2
-        (0 < nonNullCount m && nonNullCount m == i)
-        "0 < nonNullCount m && nonNullCount m == i"
-    & cover 2
-        (0 < nonNullCount m && nonNullCount m < i)
-        "0 < nonNullCount m && nonNullCount m < i"
-
-prop_drop_toList_fromList
-    :: (Ord k, Show k, Eq v, MonoidNull v, Show v)
-    => Slice k v
-    -> Property
-prop_drop_toList_fromList (Slice i m) =
-    MonoidMap.drop i m
-        === (fromList . Prelude.drop i . toList) m
-    & cover 2
-        (i == 0 && 0 < nonNullCount m)
-        "i == 0 && 0 < nonNullCount m"
-    & cover 2
-        (0 < i && i < nonNullCount m)
-        "0 < i && i < nonNullCount m"
-    & cover 2
-        (0 < nonNullCount m && nonNullCount m == i)
-        "0 < nonNullCount m && nonNullCount m == i"
-    & cover 2
-        (0 < nonNullCount m && nonNullCount m < i)
-        "0 < nonNullCount m && nonNullCount m < i"
-
-prop_splitAt_toList_fromList
-    :: (Ord k, Show k, Eq v, MonoidNull v, Show v)
-    => Slice k v
-    -> Property
-prop_splitAt_toList_fromList (Slice i m) =
-    MonoidMap.splitAt i m
-        === (bimap fromList fromList . Prelude.splitAt i . toList) m
-    & cover 2
-        (i == 0 && 0 < nonNullCount m)
-        "i == 0 && 0 < nonNullCount m"
-    & cover 2
-        (0 < i && i < nonNullCount m)
-        "0 < i && i < nonNullCount m"
-    & cover 2
-        (0 < nonNullCount m && nonNullCount m == i)
-        "0 < nonNullCount m && nonNullCount m == i"
-    & cover 2
-        (0 < nonNullCount m && nonNullCount m < i)
-        "0 < nonNullCount m && nonNullCount m < i"
 
 --------------------------------------------------------------------------------
 -- Mapping
---
+++ src/test/Data/Total/MonoidMap/TypeSpec/SliceSpec.hs
@@ -0,0 +1,159 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+{- HLINT ignore "Redundant bracket" -}
+{- HLINT ignore "Use camelCase" -}
+{- HLINT ignore "Use null" -}
+
+-- |
+-- Copyright: © 2022–2023 Jonathan Knowles
+-- License: Apache-2.0
+--
+module Data.Total.MonoidMap.TypeSpec.SliceSpec
+    ( spec
+    ) where
+
+import Prelude
+
+import Data.Bifunctor
+    ( Bifunctor (bimap) )
+import Data.Function
+    ( (&) )
+import Data.Monoid
+    ( Dual, Sum (..) )
+import Data.Monoid.Null
+    ( MonoidNull )
+import Data.Proxy
+    ( Proxy (..) )
+import Data.Set
+    ( Set )
+import Data.Text
+    ( Text )
+import Data.Total.MonoidMap
+    ( MonoidMap, nonNullCount )
+import Data.Typeable
+    ( typeRep )
+import GHC.Exts
+    ( IsList (..) )
+import Numeric.Natural
+    ( Natural )
+import Test.Common
+    ( Key, TestConstraints, property )
+import Test.Hspec
+    ( Spec, describe, it )
+import Test.QuickCheck
+    ( Arbitrary (..), Gen, Property, choose, cover, oneof, (===) )
+
+import qualified Data.Total.MonoidMap as MonoidMap
+
+spec :: Spec
+spec = describe "Slicing" $ do
+
+    specFor (Proxy @Key) (Proxy @(Set Int))
+    specFor (Proxy @Key) (Proxy @(Set Natural))
+    specFor (Proxy @Key) (Proxy @(Sum Int))
+    specFor (Proxy @Key) (Proxy @(Sum Natural))
+    specFor (Proxy @Key) (Proxy @[Int])
+    specFor (Proxy @Key) (Proxy @[Natural])
+    specFor (Proxy @Key) (Proxy @(Text))
+    specFor (Proxy @Key) (Proxy @(Dual [Int]))
+    specFor (Proxy @Key) (Proxy @(Dual [Natural]))
+    specFor (Proxy @Key) (Proxy @(Dual Text))
+
+specFor
+    :: forall k v. TestConstraints k v
+    => Proxy k
+    -> Proxy v
+    -> Spec
+specFor _k _v = describe (show $ typeRep (Proxy @(MonoidMap k v))) $ do
+
+    it "prop_take_toList_fromList" $
+        prop_take_toList_fromList
+            @k @v & property
+    it "prop_drop_toList_fromList" $
+        prop_drop_toList_fromList
+            @k @v & property
+    it "prop_splitAt_toList_fromList" $
+        prop_splitAt_toList_fromList
+            @k @v & property
+
+data Slice k v = Slice Int (MonoidMap k v)
+    deriving (Eq, Show)
+
+instance (Arbitrary k, Arbitrary v, MonoidNull v, Ord k) =>
+    Arbitrary (Slice k v)
+  where
+    arbitrary = do
+        m <- genMap
+        i <- genIndex m
+        pure $ Slice i m
+      where
+        genMap :: Gen (MonoidMap k v)
+        genMap = arbitrary
+
+        genIndex :: MonoidMap k v -> Gen Int
+        genIndex m = oneof
+            [ choose (negate (length m), -1)
+            , pure 0
+            , choose (1, length m - 1)
+            , pure (length m)
+            , choose (length m + 1, 2 * length m)
+            ]
+
+prop_take_toList_fromList
+    :: (Ord k, Show k, Eq v, MonoidNull v, Show v)
+    => Slice k v
+    -> Property
+prop_take_toList_fromList (Slice i m) =
+    MonoidMap.take i m
+        === (fromList . Prelude.take i . toList) m
+    & cover 2
+        (i == 0 && 0 < nonNullCount m)
+        "i == 0 && 0 < nonNullCount m"
+    & cover 2
+        (0 < i && i < nonNullCount m)
+        "0 < i && i < nonNullCount m"
+    & cover 2
+        (0 < nonNullCount m && nonNullCount m == i)
+        "0 < nonNullCount m && nonNullCount m == i"
+    & cover 2
+        (0 < nonNullCount m && nonNullCount m < i)
+        "0 < nonNullCount m && nonNullCount m < i"
+
+prop_drop_toList_fromList
+    :: (Ord k, Show k, Eq v, MonoidNull v, Show v)
+    => Slice k v
+    -> Property
+prop_drop_toList_fromList (Slice i m) =
+    MonoidMap.drop i m
+        === (fromList . Prelude.drop i . toList) m
+    & cover 2
+        (i == 0 && 0 < nonNullCount m)
+        "i == 0 && 0 < nonNullCount m"
+    & cover 2
+        (0 < i && i < nonNullCount m)
+        "0 < i && i < nonNullCount m"
+    & cover 2
+        (0 < nonNullCount m && nonNullCount m == i)
+        "0 < nonNullCount m && nonNullCount m == i"
+    & cover 2
+        (0 < nonNullCount m && nonNullCount m < i)
+        "0 < nonNullCount m && nonNullCount m < i"
+
+prop_splitAt_toList_fromList
+    :: (Ord k, Show k, Eq v, MonoidNull v, Show v)
+    => Slice k v
+    -> Property
+prop_splitAt_toList_fromList (Slice i m) =
+    MonoidMap.splitAt i m
+        === (bimap fromList fromList . Prelude.splitAt i . toList) m
+    & cover 2
+        (i == 0 && 0 < nonNullCount m)
+        "i == 0 && 0 < nonNullCount m"
+    & cover 2
+        (0 < i && i < nonNullCount m)
+        "0 < i && i < nonNullCount m"
+    & cover 2
+        (0 < nonNullCount m && nonNullCount m == i)
+        "0 < nonNullCount m && nonNullCount m == i"
+    & cover 2
+        (0 < nonNullCount m && nonNullCount m < i)
+        "0 < nonNullCount m && nonNullCount m < i"
