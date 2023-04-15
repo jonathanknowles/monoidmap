0b8b3af720296d047ef376831ce62666642e4a64
Jonathan Knowles <mail@jonathanknowles.net>
2023-04-15 10:54:32 +0000

    Create `ConversionSpec`.

--- src/test/Data/Total/MonoidMap/TypeSpec.hs
+++ src/test/Data/Total/MonoidMap/TypeSpec.hs
@@ -17,8 +17,6 @@ import Data.Function
     ( (&) )
 import Data.Functor.Identity
     ( Identity (..) )
-import Data.Map.Strict
-    ( Map )
 import Data.Monoid
     ( Dual, Sum (..) )
 import Data.Monoid.Null
@@ -42,10 +40,6 @@ import Test.Hspec
 import Test.QuickCheck
     ( Fun (..), Property, applyFun2, cover, expectFailure, (===) )
 
-import qualified Data.Foldable as F
-import qualified Data.List as List
-import qualified Data.List.NonEmpty as NE
-import qualified Data.Map.Strict as Map
 import qualified Data.Monoid.Null as Null
 import qualified Data.Set as Set
 import qualified Data.Total.MonoidMap as MonoidMap
@@ -70,34 +64,6 @@ specFor
     -> Proxy v
     -> Spec
 specFor _k _v = describe (show $ typeRep (Proxy @(MonoidMap k v))) $ do
-
-    describe "Conversion to and from lists" $ do
-        it "prop_fromList_get" $
-            prop_fromList_get
-                @k @v & property
-        it "prop_fromList_toMap" $
-            prop_fromList_toMap
-                @k @v & property
-        it "prop_fromList_toList" $
-            prop_fromList_toList
-                @k @v & property
-        it "prop_toList_fromList" $
-            prop_toList_fromList
-                @k @v & property
-        it "prop_toList_sort" $
-            prop_toList_sort
-                @k @v & property
-        it "prop_fromListWith_get" $
-            prop_fromListWith_get
-                @k @v & property
-
-    describe "Conversion to and from ordinary maps" $ do
-        it "prop_fromMap_toMap" $
-            prop_fromMap_toMap
-                @k @v & property
-        it "prop_toMap_fromMap" $
-            prop_toMap_fromMap
-                @k @v & property
 
     describe "Singleton" $ do
         it "prop_singleton_get" $
@@ -192,141 +158,6 @@ specFor _k _v = describe (show $ typeRep (Proxy @(MonoidMap k v))) $ do
         it "prop_append_get" $
             prop_append_get
                 @k @v & property
-
---------------------------------------------------------------------------------
--- Conversion to and from lists
---------------------------------------------------------------------------------
-
-prop_fromList_get
-    :: (Ord k, Show k, Eq v, MonoidNull v, Show v)
-    => [(k, v)]
-    -> k
-    -> Property
-prop_fromList_get kvs k =
-    MonoidMap.get k (MonoidMap.fromList kvs)
-        ===
-        F.foldMap snd (filter ((== k) . fst) kvs)
-    & cover 2
-        (matchingKeyCount == 0)
-        "matchingKeyCount == 0"
-    & cover 2
-        (matchingKeyCount == 1)
-        "matchingKeyCount == 1"
-    & cover 2
-        (matchingKeyCount == 2)
-        "matchingKeyCount == 2"
-    & cover 2
-        (matchingKeyCount >= 3)
-        "matchingKeyCount >= 3"
-  where
-    matchingKeyCount =
-        length $ filter ((== k) . fst) kvs
-
-prop_fromList_toMap
-    :: (Ord k, Show k, Eq v, MonoidNull v, Show v)
-    => [(k, v)]
-    -> Property
-prop_fromList_toMap kvs =
-    MonoidMap.toMap m === Map.filter (/= mempty) o
-    & cover 2
-        (MonoidMap.nonNull m && nonNullCount m /= Map.size o)
-        "MonoidMap.nonNull m && nonNullCount m /= Map.size o"
-    & cover 2
-        (MonoidMap.nonNull m && nonNullCount m == Map.size o)
-        "MonoidMap.nonNull m && nonNullCount m == Map.size o"
-  where
-    m = MonoidMap.fromList kvs
-    o = Map.fromListWith (flip (<>)) kvs
-
-prop_fromList_toList
-    :: (Ord k, Show k, Eq v, MonoidNull v, Show v)
-    => [(k, v)]
-    -> Property
-prop_fromList_toList kvs =
-    MonoidMap.toList m === Map.toList (Map.filter (/= mempty) o)
-    & cover 2
-        (MonoidMap.nonNull m && nonNullCount m /= Map.size o)
-        "MonoidMap.nonNull m && nonNullCount m /= Map.size o"
-    & cover 2
-        (MonoidMap.nonNull m && nonNullCount m == Map.size o)
-        "MonoidMap.nonNull m && nonNullCount m == Map.size o"
-  where
-    m = MonoidMap.fromList kvs
-    o = Map.fromListWith (flip (<>)) kvs
-
-prop_toList_fromList
-    :: (Ord k, Show k, Eq v, MonoidNull v, Show v)
-    => MonoidMap k v
-    -> Property
-prop_toList_fromList m =
-    MonoidMap.fromList (MonoidMap.toList m) === m
-    & cover 2
-        (MonoidMap.nonNull m)
-        "MonoidMap.nonNull m"
-
-prop_toList_sort
-    :: (Ord k, Show k, Eq v, MonoidNull v, Show v)
-    => MonoidMap k v
-    -> Property
-prop_toList_sort m =
-    List.sortOn fst (MonoidMap.toList m) === MonoidMap.toList m
-    & cover 2
-        (MonoidMap.nonNull m)
-        "MonoidMap.nonNull m"
-
-prop_fromListWith_get
-    :: (Ord k, Show k, Eq v, MonoidNull v, Show v)
-    => Fun (v, v) v
-    -> [(k, v)]
-    -> k
-    -> Property
-prop_fromListWith_get (applyFun2 -> f) kvs k =
-    MonoidMap.get k (MonoidMap.fromListWith f kvs)
-        ===
-        maybe mempty
-            (F.foldl1 f)
-            (NE.nonEmpty (snd <$> filter ((== k) . fst) kvs))
-    & cover 2
-        (matchingKeyCount == 0)
-        "matchingKeyCount == 0"
-    & cover 2
-        (matchingKeyCount == 1)
-        "matchingKeyCount == 1"
-    & cover 2
-        (matchingKeyCount == 2)
-        "matchingKeyCount == 2"
-    & cover 2
-        (matchingKeyCount >= 3)
-        "matchingKeyCount >= 3"
-  where
-    matchingKeyCount =
-        length $ filter ((== k) . fst) kvs
-
---------------------------------------------------------------------------------
--- Conversion to and from ordinary maps
---------------------------------------------------------------------------------
-
-prop_fromMap_toMap
-    :: (Ord k, Show k, Eq v, MonoidNull v, Show v)
-    => Map k v
-    -> Property
-prop_fromMap_toMap o =
-    MonoidMap.toMap m === Map.filter (/= mempty) o
-    & cover 2
-        (MonoidMap.nonNull m && nonNullCount m /= Map.size o)
-        "MonoidMap.nonNull m && nonNullCount m /= Map.size o"
-    & cover 2
-        (MonoidMap.nonNull m && nonNullCount m == Map.size o)
-        "MonoidMap.nonNull m && nonNullCount m == Map.size o"
-  where
-    m = MonoidMap.fromMap o
-
-prop_toMap_fromMap
-    :: (Ord k, Show k, Eq v, MonoidNull v, Show v)
-    => MonoidMap k v
-    -> Property
-prop_toMap_fromMap m =
-    MonoidMap.fromMap (MonoidMap.toMap m) === m
 
 --------------------------------------------------------------------------------
 -- Singleton
