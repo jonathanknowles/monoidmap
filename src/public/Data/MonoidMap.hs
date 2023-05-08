b4e03e96b515b69524353910f13b95ade26219d3
Jonathan Knowles <mail@jonathanknowles.net>
2023-05-08 06:04:54 +0000

    Revise descriptions of properties for monoidal operations.

--- src/public/Data/MonoidMap.hs
+++ src/public/Data/MonoidMap.hs
@@ -360,15 +360,13 @@ import qualified Data.Monoid.Null as C
 -- - /binary/ operations on /pairs of maps/ are defined in terms of their
 --   distributive application to all /pairs of values/ for matching keys.
 --
--- /Unary/ monoidal operations typically satisfy a property of the following
--- form:
+-- /Unary/ monoidal operations typically satisfy a property similar to:
 --
 -- @
 -- ∀ k. 'get' k (f m) '==' f ('get' k m)
 -- @
 --
--- /Binary/ monoidal operations typically satisfy a property of the following
--- form:
+-- /Binary/ monoidal operations typically satisfy a property similar to:
 --
 -- @
 -- ∀ k. 'get' k (f m1 m2) '==' f ('get' k m1) ('get' k m2)
