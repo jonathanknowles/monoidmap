f3012430e3f55c8d43d271bee32c71d89e22335b
Jonathan Knowles <mail@jonathanknowles.net>
2025-02-22 03:13:32 +0000

    Add module documentation for 'Data.MonoidMap.JSON'.

--- components/monoidmap-aeson/Data/MonoidMap/JSON.hs
+++ components/monoidmap-aeson/Data/MonoidMap/JSON.hs
@@ -4,7 +4,53 @@
 -- Copyright: © 2022–2025 Jonathan Knowles
 -- License: Apache-2.0
 --
-module Data.MonoidMap.JSON where
+-- This module provides instances of 'ToJSON' and 'FromJSON' for 'MonoidMap'.
+--
+-- These instances provide objects of type 'MonoidMap' __@k@__ __@v@__ with a
+-- JSON encoding identical to that of 'Map' __@k@__ __@v@__, serialised as
+-- either JSON /objects/ or /arrays/ depending on the key type __@k@__.
+--
+-- = Examples
+--
+-- === Encoding as JSON objects
+--
+-- @
+-- >>> 'encode' '$' 'fromList' [("abc", 'Sum' 1), ("def", 'Sum' 2)]
+-- "{\\"abc\\":1,\\"def\\":2}"
+-- @
+--
+-- === Encoding as JSON arrays
+--
+-- @
+-- >>> 'encode' '$' 'fromList' [((1,2), 'Sum' 3), ((2,3), 'Sum' 5)]
+-- "[[[1,2],3],[[2,3],5]]"
+-- @
+--
+-- = Laws
+--
+-- == Decoding from JSON
+--
+-- The 'FromJSON' instance satisfies the following law:
+--
+-- @
+-- 'parseJSON' '==' 'fmap' ('fmap' 'MonoidMap'.'fromMap') 'parseJSON'
+-- @
+--
+-- Mappings from keys to values that decode to 'mempty' are __not__ included in
+-- decoded 'MonoidMap' objects.
+--
+-- == Encoding to JSON
+--
+-- The 'ToJSON' instance satisfies the following laws:
+--
+-- @
+-- 'toEncoding' '==' 'toEncoding' '.' 'MonoidMap'.'toMap'
+-- 'toJSON'     '==' 'toJSON'     '.' 'MonoidMap'.'toMap'
+-- @
+--
+module Data.MonoidMap.JSON
+    ()
+    where
 
 import Prelude
 
