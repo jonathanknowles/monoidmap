ff3d912bca97f5669f24b9ec5985620a9354570f
Jonathan Knowles <mail@jonathanknowles.net>
2026-01-19 20:32:33 +0800

    Bump copyright year.

--- components/monoidmap/Data/MonoidMap.hs
+++ components/monoidmap/Data/MonoidMap.hs
@@ -1,7 +1,7 @@
 {-# OPTIONS_GHC -fno-warn-unused-imports #-}
 
 -- |
--- Copyright: © 2022–2025 Jonathan Knowles
+-- Copyright: © 2022–2026 Jonathan Knowles
 -- License: Apache-2.0
 --
 module Data.MonoidMap
